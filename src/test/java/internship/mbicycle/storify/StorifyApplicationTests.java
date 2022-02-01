6bf7b741efb0e4c674f69fee7c84a2950d0d063c
Estrin Vladislav <estrynvladislav@gmail.com>
2022-02-01 15:21:09 +0300

    initial commit

---
+++ src/test/java/internship/mbicycle/storify/StorifyApplicationTests.java
@@ -0,0 +1,13 @@
+package internship.mbicycle.storify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
