effcbbb1583ec2529fd00f2b610176dea9833d71
andrey11m <andrey.shkurko@mbicycle.com>
2022-02-03 15:54:05 +0300

    added StorifyUser

---
+++ src/main/java/internship/mbicycle/storify/entity/StorifyUser.java
@@ -0,0 +1,18 @@
+package internship.mbicycle.storify.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class StorifyUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
