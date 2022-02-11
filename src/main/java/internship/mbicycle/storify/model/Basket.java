37b84cacc9ad92ce89aa8954671a2d89afb2a2bc
Harbachou Aliaksandr <aliaksandr.harbachou@mbicycle.com>
2022-02-11 13:59:21 +0300

    add Entity Basket

--- src/main/java/internship/mbicycle/storify/model/Basket.java
+++ src/main/java/internship/mbicycle/storify/model/Basket.java
@@ -1,5 +1,39 @@
 package internship.mbicycle.storify.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Aliaksandr Harbachou
+ */
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "basket")
 public class Basket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "product_basket",
+            joinColumns = @JoinColumn(
+                    name = "product_id",
+                    referencedColumnName = "id"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "basket_id",
+                    referencedColumnName = "id"
+            )
+    )
+    private List<Product> productList = new ArrayList<>();
 
 }
