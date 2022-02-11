7c4f1a700cc4c531f17d60566ace83d6039814ea
VasiliZas <vasili.zasinets@gmail.com>
2022-02-11 14:06:06 +0300

    Add entity Product

---
+++ src/main/java/internship/mbicycle/storify/model/Product.java
@@ -0,0 +1,42 @@
+package internship.mbicycle.storify.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    private String image;
+
+    private String description;
+
+    private Integer price;
+
+    private Integer count;
+
+    @Column(name = "store_id")
+    private Long storeId;
+
+}
