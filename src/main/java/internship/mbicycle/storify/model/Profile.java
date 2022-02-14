06cef9c7cfeb5ea33becc8809067836fdb26a224
zaiceva <mariya.zaiceva@mbicycle.com>
2022-02-14 11:22:16 +0300

    added cascade type to profile

--- src/main/java/internship/mbicycle/storify/model/Profile.java
+++ src/main/java/internship/mbicycle/storify/model/Profile.java
@@ -25,26 +25,23 @@ public class Profile {
     private String address;
     private String phone;
 
-    /*Need entities
-
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
     @JoinColumn(name = "basket_id")
     @JsonIgnore
     private Basket basket;
 
-    @OneToMany(mappedBy = "profile", fetch = FetchType.LAZY)
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "profile", fetch = FetchType.LAZY)
     @JsonIgnore
     private List<Store> stores;
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.REFRESH, CascadeType.MERGE}, fetch = FetchType.LAZY)
     @JoinTable(name="favorite",
             joinColumns={@JoinColumn(name="profile_id")},
             inverseJoinColumns={@JoinColumn(name="product_id")})
-    @JsonIgnore
     private List<Product> favorite;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "profile", fetch = FetchType.LAZY)
     @JsonIgnore
-    private List<Order> orders;*/
+    private List<Order> orders;
 
 }
