da7ec4b37fc2f47837226e0355288a41da5fd39a
Harbachou Aliaksandr <aliaksandr.harbachou@mbicycle.com>
2022-02-14 11:43:06 +0300

    fix Basket and fix tableBasket

--- src/main/java/internship/mbicycle/storify/model/Basket.java
+++ src/main/java/internship/mbicycle/storify/model/Basket.java
@@ -1,39 +1,35 @@
 package internship.mbicycle.storify.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.List;
-
-/**
- * @author Aliaksandr Harbachou
- */
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Data
-@Table(name = "basket")
 public class Basket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToOne(name = "profile_id", fetch = FetchType.LAZY)
+    private Profile profile;
+
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinTable(
             name = "product_basket",
             joinColumns = @JoinColumn(
-                    name = "product_id",
-                    referencedColumnName = "id"
+                    name = "product_id"
             ),
             inverseJoinColumns = @JoinColumn(
-                    name = "basket_id",
-                    referencedColumnName = "id"
+                    name = "basket_id"
             )
     )
-    private List<Product> productList = new ArrayList<>();
-
+    private List<Product> productList;
 }
