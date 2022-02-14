d3c572c1b608bd82b0e81cce9547edf89b89c131
VasiliBlr <98832547+VasiliBlr@users.noreply.github.com>
2022-02-14 15:49:27 +0300

    Add ProductDto (#40)
    * Add ProductDto
    
    * Fix name ProductDTO
    
    Co-authored-by: Alexander <bad.bad.jaguar@gmail.com>

---
+++ src/main/java/internship/mbicycle/storify/dto/ProductDTO.java
@@ -0,0 +1,22 @@
+package internship.mbicycle.storify.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+
+    private Long id;
+    private String productName;
+    private String image;
+    private String description;
+    private Integer price;
+    private Integer count;
+    private Long storeId;
+
+}
