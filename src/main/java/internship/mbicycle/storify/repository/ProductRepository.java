09ac7e6c696f55c634dd8f6c01cebffc28958525
VasiliBlr <98832547+VasiliBlr@users.noreply.github.com>
2022-02-14 17:01:06 +0300

    Added Product and Order Repository (#43)
    Co-authored-by: Alexander <bad.bad.jaguar@gmail.com>

---
+++ src/main/java/internship/mbicycle/storify/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package internship.mbicycle.storify.repository;
+
+import internship.mbicycle.storify.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findProductByName(String name);
+
+}
