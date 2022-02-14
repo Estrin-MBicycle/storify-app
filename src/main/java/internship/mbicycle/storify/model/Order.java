afc1ae614c75d3e47512fe4205ed579cff0a1d35
VasiliBlr <98832547+VasiliBlr@users.noreply.github.com>
2022-02-14 15:35:17 +0300

    Add many to many Order to Product (#38)
    * Add many to many Order to Product
    
    * Fix PR remark
    
    Co-authored-by: Alexander <bad.bad.jaguar@gmail.com>

--- src/main/java/internship/mbicycle/storify/model/Order.java
+++ src/main/java/internship/mbicycle/storify/model/Order.java
@@ -1,16 +1,22 @@
 package internship.mbicycle.storify.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import java.time.LocalDate;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Builder
@@ -26,12 +32,20 @@ public class Order {
     @Column(name = "order_date")
     private LocalDate date;
 
-    private  Integer price;
+    private Integer price;
 
     @Column(name = "unique_code")
     private String uniqueCode;
 
     @Column(name = "profile_id")
     private Long profileId;
+
+    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH,
+            CascadeType.PERSIST})
+    @JoinTable(
+            name = "product_order",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id"))
+    private List<Product> productList = new ArrayList<>();
 
 }
