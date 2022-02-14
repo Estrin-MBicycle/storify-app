1db9d8e6dcfda0fb66b710f5ce9940affb6afd04
Grigorovich <veronika.grigorovich@mbicycle.com>
2022-02-14 10:54:26 +0300

    Corrected comments, added annotations in class StoreDTO

--- src/main/java/internship/mbicycle/storify/dto/StoreDTO.java
+++ src/main/java/internship/mbicycle/storify/dto/StoreDTO.java
@@ -1,9 +1,15 @@
 package internship.mbicycle.storify.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class StoreDto {
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StoreDTO {
     private Long id;
     private String storeName;
     private String description;
