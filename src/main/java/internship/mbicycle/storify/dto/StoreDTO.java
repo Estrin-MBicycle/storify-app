c2902bb647a1d54a6a9b2e81858b8b33c690818d
grigveronika <98822041+grigveronika@users.noreply.github.com>
2022-02-14 11:05:21 +0300

    created  StoreDTO (#30)
    * created  StoreDTO
    
    * Corrected comments, added annotations in class StoreDTO

---
+++ src/main/java/internship/mbicycle/storify/dto/StoreDTO.java
@@ -0,0 +1,22 @@
+package internship.mbicycle.storify.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StoreDTO {
+    private Long id;
+    private String storeName;
+    private String description;
+    private String address;
+    private Long profit;
+
+// нужен ProfileDto
+//    private ProfileDto profileDto;
+
+}
