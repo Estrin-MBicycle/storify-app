fa9471a70d5307721b746cb9ac86b6e3644eca97
VasiliBlr <98832547+VasiliBlr@users.noreply.github.com>
2022-02-14 11:04:05 +0300

    Add entity Product (#29)
    * Add entity Product
    
    * Fix remark PR
    
    Co-authored-by: VasiliZas <vasili.zasinets@gmail.com>

---
+++ src/main/java/internship/mbicycle/storify/model/Product.java
@@ -0,0 +1,40 @@
+package internship.mbicycle.storify.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    private String image;
+
+    private String description;
+
+    private Integer price;
+
+    private Integer count;
+
+    @Column(name = "store_id")
+    private Long storeId;
+
+}
