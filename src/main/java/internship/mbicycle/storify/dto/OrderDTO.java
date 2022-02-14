cd2e94c8a8a9cf52aaf872073cdb50a653d02f27
VasiliBlr <98832547+VasiliBlr@users.noreply.github.com>
2022-02-14 17:26:43 +0300

    Added OrderDTO (#42)
    Co-authored-by: Alexander <bad.bad.jaguar@gmail.com>

---
+++ src/main/java/internship/mbicycle/storify/dto/OrderDTO.java
@@ -0,0 +1,23 @@
+package internship.mbicycle.storify.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDTO {
+
+    private Long id;
+    private LocalDate date;
+    private Integer price;
+    private String uniqueCode;
+    private Long profileId;
+    private List<ProductDTO> productDTOList;
+}
