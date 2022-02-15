e8d0aa5cb467b57fd8f048dbe8ff3272e030e89c
Grigorovich <veronika.grigorovich@mbicycle.com>
2022-02-15 16:27:19 +0300

    renamed names of methods in controller

--- src/main/java/internship/mbicycle/storify/controller/StoreController.java
+++ src/main/java/internship/mbicycle/storify/controller/StoreController.java
@@ -36,13 +36,13 @@ public class StoreController {
     }
 
     @DeleteMapping("/{profileId}")
-    public ResponseEntity<?> deleteAllStores(@PathVariable Long profileId) {
+    public ResponseEntity<?> deleteAllStoresByProfileId(@PathVariable Long profileId) {
         storeService.deleteAllByProfileId(profileId);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}/{profileId}")
-    public ResponseEntity<?> deleteStore(@PathVariable Long id, @PathVariable Long profileId) {
+    public ResponseEntity<?> deleteStoreByProfileId(@PathVariable Long id, @PathVariable Long profileId) {
         storeService.deleteByIdAndProfileId(id, profileId);
         return ResponseEntity.ok().build();
     }
