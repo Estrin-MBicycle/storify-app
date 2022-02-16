796396d15eac65b31ca14e3dc919000fe356856b
VasiliBlr <98832547+VasiliBlr@users.noreply.github.com>
2022-02-16 15:18:05 +0300

    Add tests for OrderServiceImpl.convert and ProductServiceImpl.convert (#57)
    Co-authored-by: Alexander <bad.bad.jaguar@gmail.com>

--- src/test/java/internship/mbicycle/storify/service/impl/OrderServiceImplTest.java
+++ src/test/java/internship/mbicycle/storify/service/impl/OrderServiceImplTest.java
@@ -2,12 +2,14 @@ package internship.mbicycle.storify.service.impl;
 
 import internship.mbicycle.storify.dto.OrderDTO;
 import internship.mbicycle.storify.exception.ResourceNotFoundException;
+import internship.mbicycle.storify.model.Order;
 import internship.mbicycle.storify.repository.OrderRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -15,11 +17,26 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class OrderServiceImplTest {
 
     private OrderServiceImpl orderService;
+    private Order order;
+    private OrderDTO orderDTO;
 
     @BeforeEach
     void setUp() {
         orderService = new OrderServiceImpl(Mockito.mock(OrderRepository.class),
                 Mockito.mock(GeneratorUniqueCodeImpl.class));
+
+        order = Order.builder()
+                .id(87L)
+                .delivered(false)
+                .date(LocalDate.now())
+                .build();
+
+        orderDTO = OrderDTO.builder()
+                .delivered(false)
+                .id(55L)
+                .date(LocalDate.now())
+                .build();
+
     }
 
     @Test
@@ -38,5 +55,17 @@ class OrderServiceImplTest {
     void testFindByProfileId() {
         List<OrderDTO> list = orderService.getAllOrdersByProfileId(0L);
         Assertions.assertTrue(list.isEmpty());
+    }
+
+    @Test
+    void testConvertOrderToOrderDTO() {
+        OrderDTO test = OrderServiceImpl.convertOrderToDTO(order);
+        Assertions.assertInstanceOf(OrderDTO.class, test);
+    }
+
+    @Test
+    void testConvertOrderDTOToOrder() {
+        Order test = OrderServiceImpl.convertDTOToOrder(orderDTO);
+        Assertions.assertInstanceOf(Order.class, test);
     }
 }
--- src/test/java/internship/mbicycle/storify/service/impl/ProductServiceImplTest.java
+++ src/test/java/internship/mbicycle/storify/service/impl/ProductServiceImplTest.java
@@ -1,6 +1,8 @@
 package internship.mbicycle.storify.service.impl;
 
+import internship.mbicycle.storify.dto.ProductDTO;
 import internship.mbicycle.storify.exception.ResourceNotFoundException;
+import internship.mbicycle.storify.model.Product;
 import internship.mbicycle.storify.repository.ProductRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,10 +15,26 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class ProductServiceImplTest {
 
     private ProductServiceImpl productService;
+    private Product product;
+    private ProductDTO productDTO;
 
     @BeforeEach
     void setUp() {
         productService = new ProductServiceImpl(Mockito.mock(ProductRepository.class));
+
+        product = Product.builder()
+                .productName("Car")
+                .price(1000)
+                .id(89L)
+                .count(57)
+                .build();
+
+        productDTO = ProductDTO.builder()
+                .productName("Mobile")
+                .price(200)
+                .id(10L)
+                .count(5)
+                .build();
     }
 
     @Test
@@ -29,5 +47,17 @@ class ProductServiceImplTest {
         ResourceNotFoundException thrown = Assertions.assertThrows(ResourceNotFoundException.class, () ->
                 productService.getProductById(-45L));
         Assertions.assertEquals("Product not found.", thrown.getMessage());
+    }
+
+    @Test
+    void testConvertProductToProductDTO() {
+        ProductDTO test = ProductServiceImpl.convertToDTO(product);
+        Assertions.assertInstanceOf(ProductDTO.class, test);
+    }
+
+    @Test
+    void testConvertProductDTOToProduct() {
+        Product test = ProductServiceImpl.convertToProduct(productDTO);
+        Assertions.assertInstanceOf(Product.class, test);
     }
 }
