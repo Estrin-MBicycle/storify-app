9d922197f257900061677eed146db3ca54cd52e8
zaiceva <mariya.zaiceva@mbicycle.com>
2022-02-16 14:36:06 +0300

    delete cycles for, added streams

--- src/main/java/internship/mbicycle/storify/service/impl/ProfileServiceImpl.java
+++ src/main/java/internship/mbicycle/storify/service/impl/ProfileServiceImpl.java
@@ -1,16 +1,22 @@
 package internship.mbicycle.storify.service.impl;
 
 import internship.mbicycle.storify.dto.ProfileDTO;
+import internship.mbicycle.storify.dto.StoreDTO;
 import internship.mbicycle.storify.exception.ErrorCode;
 import internship.mbicycle.storify.exception.ResourceNotFoundException;
 import internship.mbicycle.storify.model.Profile;
+import internship.mbicycle.storify.model.Store;
 import internship.mbicycle.storify.repository.ProfileRepository;
 import internship.mbicycle.storify.service.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -23,7 +29,7 @@ public class ProfileServiceImpl implements ProfileService {
     public ProfileDTO getById(long id) {
         Profile temp = Optional.ofNullable(profileRepository.findById(id))
                 .orElseThrow(() -> new ResourceNotFoundException(ErrorCode.NOT_FOUND_PROFILE)).get();
-        return convertEntityToDTO(temp);
+        return convertProfileToProfileDTO(temp);
     }
 
     @Override
@@ -35,10 +41,10 @@ public class ProfileServiceImpl implements ProfileService {
         temp.setAddress(profileDTO.getAddress());
         temp.setPhone(profileDTO.getPhone());
         Profile result = profileRepository.save(temp);
-        return convertEntityToDTO(result);
+        return convertProfileToProfileDTO(result);
     }
 
-    public static ProfileDTO convertEntityToDTO(Profile profile) {
+    public static ProfileDTO convertProfileToProfileDTO(Profile profile) {
         ProfileDTO profileDTO = new ProfileDTO();
         profileDTO.setId(profile.getId());
         profileDTO.setName(profile.getName());
@@ -46,17 +52,16 @@ public class ProfileServiceImpl implements ProfileService {
         profileDTO.setTown(profile.getTown());
         profileDTO.setAddress(profile.getAddress());
         profileDTO.setPhone(profile.getPhone());
-        if(profileDTO.getStores() != null) {
-            for(int i = 0; i < profile.getStores().size(); i++) {
-                profileDTO.getStores().add(
-                        StoreServiceImpl.fromStoreToStoreDTO(
-                                profile.getStores().get(i)));
-            }
+        if(profile.getStores() != null) {
+            profileDTO.setStores(profile.getStores().stream().filter(Objects::nonNull)
+                    .map(StoreServiceImpl::fromStoreToStoreDTO).collect(Collectors.toList()));
+        } else {
+            profileDTO.setStores(new ArrayList<StoreDTO>());
         }
         return profileDTO;
     }
 
-    public static Profile convertDTOToEntity(ProfileDTO profileDTO) {
+    public static Profile convertProfileDTOToProfile(ProfileDTO profileDTO) {
         Profile profile = new Profile();
         profile.setId(profileDTO.getId());
         profile.setName(profileDTO.getName());
@@ -65,11 +70,10 @@ public class ProfileServiceImpl implements ProfileService {
         profile.setAddress(profileDTO.getAddress());
         profile.setPhone(profileDTO.getPhone());
         if(profileDTO.getStores() != null) {
-            for (int i = 0; i < profileDTO.getStores().size(); i++) {
-                profile.getStores().add(
-                        StoreServiceImpl.fromStoreDTOToStore(
-                                profileDTO.getStores().get(i)));
-            }
+            profile.setStores(profileDTO.getStores().stream().map(StoreServiceImpl::fromStoreDTOToStore)
+                    .collect(Collectors.toList()));
+        } else {
+            profile.setStores(new ArrayList<Store>());
         }
         return profile;
     }
