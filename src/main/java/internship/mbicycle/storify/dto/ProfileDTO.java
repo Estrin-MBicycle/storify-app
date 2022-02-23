9037724952919cb84075c241f643fb7c51697e94
zaiceva <mariya.zaiceva@mbicycle.com>
2022-02-23 15:30:56 +0300

    added validation profile

--- src/main/java/internship/mbicycle/storify/dto/ProfileDTO.java
+++ src/main/java/internship/mbicycle/storify/dto/ProfileDTO.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -12,10 +16,25 @@ import lombok.NoArgsConstructor;
 public class ProfileDTO {
 
     private Long id;
+
+    @NotBlank(message = "name is required field")
+    @Size(max = 50, message = "name must be max 50 symbols")
     private String name;
+
+    @NotBlank(message = "surname is required field")
+    @Size(max = 50, message = "surname must be max 50 symbols")
     private String surname;
+
+    @NotBlank(message = "town is required field")
+    @Size(max = 50, message = "town must be max 50 symbols")
     private String town;
+
+    @NotBlank(message = "address is required field")
+    @Size(max = 50, message = "address must be max 50 symbols")
     private String address;
+
+    @Pattern(regexp = "^\\+375\\s\\(\\d{2}\\)\\s\\d{3}-\\d{2}-\\d{2}$",
+            message = "phone number is not valid (+375 (11) 222-33-44)")
     private String phone;
 
     private CartDTO cartDTO;
