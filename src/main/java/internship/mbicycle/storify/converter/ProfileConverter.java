001e2c95fbf21da0db896d16bb6cdf9c55761874
Andrei Shkurko <98820909+andrey11m@users.noreply.github.com>
2022-03-10 12:36:05 +0300

    fixed bags with access to profile (#111)
    * fixed bags with access to profile
    
    * added test profile, delete method from converter and service
    
    Co-authored-by: zaiceva <mariya.zaiceva@mbicycle.com>

--- src/main/java/internship/mbicycle/storify/converter/ProfileConverter.java
+++ src/main/java/internship/mbicycle/storify/converter/ProfileConverter.java
@@ -1,6 +1,7 @@
 package internship.mbicycle.storify.converter;
 
 import internship.mbicycle.storify.dto.ProfileDTO;
+import internship.mbicycle.storify.model.Cart;
 import internship.mbicycle.storify.model.Profile;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -8,8 +9,6 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class ProfileConverter {
-
-    private final CartConverter cartConverter;
 
     public ProfileDTO convertProfileToProfileDTO(Profile profile) {
         return ProfileDTO.builder()
@@ -19,18 +18,19 @@ public class ProfileConverter {
                 .town(profile.getTown())
                 .address(profile.getAddress())
                 .phone(profile.getPhone())
-                .cartDTO(cartConverter.convertCartToCartDTO(profile.getCart()))
                 .build();
     }
 
-    public Profile convertProfileDTOToProfile(ProfileDTO profileDTO) {
+    public Profile convertProfileDTOToProfile(long profileId, Cart cart, ProfileDTO profileDTO) {
         return Profile.builder()
-                .id(profileDTO.getId())
+                .id(profileId)
                 .name(profileDTO.getName())
                 .surname(profileDTO.getSurname())
                 .town(profileDTO.getTown())
                 .address(profileDTO.getAddress())
                 .phone(profileDTO.getPhone())
+                .cart(cart)
                 .build();
     }
+
 }
